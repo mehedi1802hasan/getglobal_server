ec9b68e75b075612ee427289d0f525435c66304b
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-10-25 13:21:01 +0600

    done

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection setup
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rin8xcl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const addEmailCollection = client.db('getGlobal').collection('email');
+
+    // 1. POST to addToys
+    app.post('/addemail', async (req, res) => {
+      const addemail = req.body;
+   //   body.createdAt = new Date()
+      const result = await addEmailCollection.insertOne(addemail);
+      res.send(result);
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('edukids server is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port:${port}`);
+});
